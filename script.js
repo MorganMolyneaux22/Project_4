16e35e99a32ab22d6e0cfa49bd8d8cf62ea1d1f4
Morgan Molyneaux <92762025+MorganMolyneaux22@users.noreply.github.com>
2023-07-21 18:45:49 -0600

    Project finished
    -Mobile Friendly
    - Button to change from light-to-dark works somewhat.
    - Changed to be more vibrant with colors and other CSS style.

---
+++ script.js
@@ -0,0 +1,47 @@
+// Morgan Molyneaux, June 28, 2023, Purpose of program: Javascript to hide and unhide sections when <a> is clicked
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        document.querySelector(".active").classList.remove("active");
+        document.querySelector(this.getAttribute("href")).classList.add("active");
+    });
+});
+
+
+// Toggle light and dark mode theme by detecing click of button o navbar
+// Code is a combination of my work, GeekForGeeks and stackoverflow
+// Link: https://www.geeksforgeeks.org/how-to-switch-between-multiple-css-stylesheets-using-javascript/#
+// Link: https://stackoverflow.com/questions/11833759/add-stylesheet-to-head-using-javascript-in-body/11833777#11833777
+function toggle_theme() {
+    var theme_link = document.querySelector('link[href="dark.css"]');
+    if (theme_link) {
+        theme_link.setAttribute('href', 'light.css');
+    } else {
+        theme_link = document.createElement('link');
+        theme_link.setAttribute('rel', 'stylesheet');
+        theme_link.setAttribute('href', 'dark.css');
+        document.head.appendChild(theme_link);
+    }
+}
+
+// Attach a click event listener to the button
+var themeToggleBtn = document.getElementById('toggle_theme');
+themeToggleBtn.addEventListener('click', toggle_theme);
+
+
+// Attach a click event listener to the button
+// var themeToggleBtn = document.getElementById('toggle_theme');
+// themeToggleBtn.addEventListener('click', toggle_theme());
+
+
+// function toggle_theme(){
+//     var theme_link = document.getElementsByTagName('link')[0];
+//     if (theme_link.getAttribute('href') == 'new_style.css') {
+//         theme_link.setAttribute('href', 'style.css');
+//     } else {
+//         theme_link.setAttribute('href', 'new_style.css');
+//     }
+// }
